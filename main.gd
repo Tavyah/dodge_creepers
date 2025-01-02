6849d3a9c3a680b8a7750399dd5838a9f558d874
Tiril Marie Pedersen Løken <94623622+Tavyah@users.noreply.github.com>
2025-01-02 13:50:10 +0100

    game completed

--- main.gd
+++ main.gd
@@ -9,14 +9,22 @@ func _ready() -> void:
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
+	get_tree().call_group("mobs", "queue_free")
 	
 func new_game() -> void:
 	score = 0
 	$StartTimer.start()
 	$Player.start($StartPosition.position)
+	$HUD.update_score(score)
+	$HUD.show_message('Get ready!')
+	$Music.play()
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
